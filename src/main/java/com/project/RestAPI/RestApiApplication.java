78c9924c9cf6e53c1d39cf28efbe44d1dfa58797
Vasu Ratanpara <vasuratanpara@gmail.com>
2020-04-17 23:30:00 -0400

    Update RestApiApplication.java

--- src/main/java/com/project/RestAPI/RestApiApplication.java
+++ src/main/java/com/project/RestAPI/RestApiApplication.java
@@ -3,13 +3,16 @@ package com.project.RestAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// Added by VAsu Ratanpara
 @SpringBootApplication
 public class RestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
-		System.out.println("Rest API");
-		System.out.println("Demo");
+		//System.out.println("Rest API");
+		//System.out.println("Demo");
+		System.out.println("Vasu Ratanpara");
 	}
 
 }
